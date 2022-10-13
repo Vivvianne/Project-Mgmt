995ba1d8fc124f6afacab339b3d0d457e221f4a2
Vivvianne <ndiahav@gmail.com>
2022-10-13 10:28:56 +0300

    changedthe ports

--- public/mark_done.js
+++ public/mark_done.js
@@ -2,7 +2,7 @@ document.getElementById("done-btn").addEventListener("click", function () {
   var taskName = document.getElementById("taskName").value;
   var projectId = document.getElementById("projectId").value;
 
-  var url = `http://localhost:5000/user/done/${projectId}/${taskName}`;
+  var url = `http://localhost:8080/user/done/${projectId}/${taskName}`;
 
   fetch(url, {
     method: "post",
